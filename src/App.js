b6d719afe65e0d3c7333d98d931bf8b0dba29126
mariobaron1 <69414741+mariobaron1@users.noreply.github.com>
2021-01-03 11:34:01 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Subheader from './components/Subheader';
+import NewsTickerAnim from './components/NewsTickerAnim';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Info from './components/Info';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Subheader />
+      <NewsTickerAnim />
+      <About />
+      <Skills />
+      <Projects />
+      <Info />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
